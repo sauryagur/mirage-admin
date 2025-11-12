f322138a8d8712b9d3d1cf7c036691e171441bba
Saurya Gur <saurya.gur@outlook.com>
2025-11-12 13:10:46 +0530

    logs ts proof

--- src/components/Logs.tsx
+++ src/components/Logs.tsx
@@ -1,3 +1,10 @@
-export default function Logs() {
-  <></>;
-}
+const Logs = () => {
+  return (
+    <div className="mt-4">
+      <h3>Logs</h3>
+      <p>Logs component is under construction.</p>
+    </div>
+  );
+};
+
+export default Logs;
